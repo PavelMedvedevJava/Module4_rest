5ce15a9ae71a52ae3e860721409aef8602b5bcf2
Pavel_Medvedev <Pavel_Medvedev@epam.com>
2023-11-08 20:07:26 +0100

    initial

---
+++ src/main/java/com/example/module4_rest/Module4RestApplication.java
@@ -0,0 +1,13 @@
+package com.example.module4_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module4RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/module4_rest/Module4RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.module4_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
